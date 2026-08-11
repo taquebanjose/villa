84052753107c1f7f15513a9cc8b30f031a8ecf6e
taqueban_jm <taquebanjose@gmail.com>
2026-08-11 18:45:18 +0800

    Update to external postgres connection

--- db/connection.php
+++ db/connection.php
@@ -1,12 +1,12 @@
 <?php
-$host = 'dpg-d9tel0h42hec7381noug-a';
+$host = 'dpg-d9tel0h42hec7381noug-a.singapore-postgres.render.com';
 $port = '5432';
 $dbname = 'villa_db_7x7e';
 $user = 'villa_db_7x7e_user';
 $password = 'Ok68wkiOIHU7cTbjO3i368QsNyH7oSrU';
 
 try {
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
     $pdo = new PDO($dsn, $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
