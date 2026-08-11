5bda99b7d3dbae72de9c867497c3d786c9b5f0a3
taqueban_jm <taquebanjose@gmail.com>
2026-08-12 02:20:55 +0800

    Update login redirect to point to policy.php

--- login.php
+++ login.php
@@ -19,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($user && password_verify($password, $user['password'])) {
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['user_name'] = $user['name'];
-            header("Location: index.php");
+            header("Location: policy.php");
             exit();
         } else {
             $error = "Invalid email or password.";
